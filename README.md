a50a1bbcce4f8573698a1e8f96a03f87aa888579
marcell <marcell.ramos@gmail.com>
2023-01-03 17:00:03 -0400

    Projeto_Help

---
+++ README.md
@@ -0,0 +1,32 @@
+![cover](.github/cover.gif?style=flat)
+
+
+## Projeto
+A aplicação para gerenciar em tempo real solicitações de manutenção e suporte a equipamentos.
+
+
+## Stack
+
+- React Native
+- Typescript
+- Expo Bare Workflow
+- Native Base
+- Firebase
+- Firebase Auth
+- Firebase Firestore with Real Time updates
+- And more...
+
+
+## Funcionalidades
+
+- Autenticação de usuários
+- Criar novas solicitações
+- Filtra solicitações pelo status
+- Encerra solicitação com solução
+- Sincronização com banco de dados em tempo real
+- Entre outras...
+
+## Contato 
+ 
+[![Linkedin Badge](https://img.shields.io/badge/-Marcell%20Ramos%20de%20Souza-6633cc?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/marcellsouza/)](https://www.linkedin.com/in/marcellsouza/) 
+</div>
---
+++ src/styles/theme.ts
@@ -0,0 +1,60 @@
+import { extendTheme } from 'native-base'
+
+export const THEME = extendTheme({
+  colors: {
+    primary: {
+      700: '#996DFF',
+    },
+    secondary: {
+      700: '#FBA94C',
+    },
+    green: {
+      700: '#00875F',
+      500: '#00B37E',
+      300: '#04D361',
+    },
+    gray: {
+      700: '#121214',
+      600: '#202024',
+      500: '#29292E',
+      400: '#323238',
+      300: '#7C7C8A',
+      200: '#C4C4CC',
+      100: '#E1E1E6',
+    },
+    white: '#FFFFFF',
+  },
+  fonts: {
+    heading: 'Roboto_700Bold',
+    body: 'Roboto_400Regular',
+  },
+  fontSizes: {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 20,
+  },
+  sizes: {
+    14: 56,
+  },
+  fontWeights: {
+    hairline: 100,
+    thin: 200,
+    light: 300,
+    normal: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+    black: 900,
+    extraBlack: 950,
+  },
+  letterSpacings: {
+    xs: '-0.05em',
+    sm: '-0.025em',
+    md: 0,
+    lg: '0.025em',
+    xl: '0.3em',
+  },
+})
